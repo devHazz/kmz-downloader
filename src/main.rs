0b40c439a561ace1afd0ee3933c4e16429c0cc0d
Harrison Dowding <harrisondcrafter@gmail.com>
2023-03-30 01:21:13 +0100

    main: add ability to download records, kmz file checks

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
+use kmz_downloader::config::Config;
 use kmz_downloader::scraper::Listing;
-use yaml_rust::YamlLoader;
-use std::fs;
 #[tokio::main]
 async fn main() {
-    let s = fs::read_to_string("config.yml").expect("could not read config file");
-    let config = YamlLoader::load_from_str(&s).unwrap();
-    let dir_url = config[0]["dir_url"].as_str().expect("could not get kmz directory url");
+    let dir_url = Config::read().expect("could not read config").dir_url;
     if !dir_url.is_empty() {
-        Listing::default().read(dir_url.to_string()).await;
+        let listing = Listing::default().read(dir_url.to_string()).await.expect("could not get directory listing");
+        println!("{:?}", listing);
+        listing.records[1].download().await;
     } else {
         panic!("config directory url: empty")
     }
