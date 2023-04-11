14a738b87c8eba488e1437223cfc9787f45710a3
Harrison Dowding <harrisondcrafter@gmail.com>
2023-04-11 12:24:57 +0100

    :sparkles: add kml read function stub & patched lib

--- src/kmz.rs
+++ src/kmz.rs
@@ -1,10 +1,12 @@
 use std::{
-    fs::{self, File},
+    fs::{self, File, OpenOptions},
+    io,
     path::Path,
-    str::Bytes, io,
+    str::Bytes,
 };
 
 use anyhow::Result;
+use kml::{KmlReader, KmlDocument};
 
 pub struct CompressedKMZ {
     path: String,
@@ -19,15 +21,30 @@ impl CompressedKMZ {
         }
     }
     pub fn unpack(&self) {
-            let file = match File::open(&self.path) {
-                Ok(file) => file,
-                Err(e) => panic!("file create error: {}", e),
-            };
-            let mut archive = zip::ZipArchive::new(file).expect("could not parse zip archive");
-            for i in 0..archive.len() {
-                let file = archive.by_index(i).expect("could not get archive");
-                println!("{}", file.name());
-                let kmz_archive = zip::ZipArchive::new(file).expect("could not read kmz archive");
+        let file = match File::open(&self.path) {
+            Ok(file) => file,
+            Err(e) => panic!("file create error: {}", e),
+        };
+        let mut archive = zip::ZipArchive::new(file).expect("could not parse zip archive");
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i).expect("could not get archive");
+            if file.name().contains("/") {
+                let folder: Vec<&str> = file.name().split("/").collect();
+                fs::create_dir_all(format!("temp/{}/", folder[0]))
+                    .expect("could not create directory");
             }
+            if file.name().ends_with("kmz") {
+                let mut out_archive = OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .create(true)
+                    .open(format!("temp/{}", file.name()))
+                    .expect("could not create temp archive");
+                io::copy(&mut file, &mut out_archive).unwrap();
+                let mut reader = KmlReader::<_, f64>::from_kmz_path(format!("temp/{}", file.name())).unwrap();
+                let kml = reader.read().expect("could not read kml");
+                println!("{:?}", kml);
+            }
+        }
     }
 }
